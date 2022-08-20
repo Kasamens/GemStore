1337a73ad2df0d87277dd5d381c4e574922dfcee
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-08-20 15:13:21 +0000

    Added get all gems and add new gem endpoints

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 from fastapi import FastAPI
 from database import create_db
-import models.gem_models
+import models.gem_models as gem_models
 import uvicorn
+from sqlmodel import Session, select
+from database import engine
+from typing import List
 
 app = FastAPI()
 
 create_db()
 
 
-@app.get("/")
-def hello():
-    return "Hello World"
+@app.get("/gems")
+def get_gems() -> List[gem_models.Gem]:
+    with Session(engine) as session:
+        statement = select(gem_models.Gem)
+        result = session.exec(statement)
+        return result.all()
+
+
+@app.post("/gems/{gem}")
+def add_gem(new_gem: gem_models.Gem, gem_props: gem_models.GemProperties):
+
+    gem_props = gem_models.GemProperties(
+        gemsize=gem_props.size, clarity=gem_props.clarity, color=gem_props.color)
+    gem = gem_models.Gem(price=new_gem.price, available=new_gem.available,
+                         gem_type=new_gem.gem_type, gem_properties=gem_props)
+
+    with Session(engine) as session:
+        session.add(gem)
+        session.commit()
+        # session.add(gem_props)
+        # session.commit()
+        session.close()
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='localhost', port=9000)
+    uvicorn.run(app, host='localhost', port=8070)
--- models/gem_models.py
+++ models/gem_models.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
 from enum import Enum
 
@@ -30,13 +30,17 @@ class GemProperties(SQLModel, table=True):
     size: float
     clarity: Optional[GemClarity] = None
     color: Optional[GemColor] = None
+    gem_properties: Optional['Gem'] = Relationship(
+        back_populates='gem_properties')
 
 
 class Gem(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True)
     price: float
-    availabile: bool = True
+    available: bool = True
     gem_type: GemType = GemType.EMERALD
 
     gem_properties_id: Optional[int] = Field(
         default=None, foreign_key='gemproperties.id')
+    gem_properties: Optional[GemProperties] = Relationship(
+        back_populates='gem')
